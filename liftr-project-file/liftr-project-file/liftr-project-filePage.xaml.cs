de28aba992062c76d6699cff21392811ed5b4360
rileydnorris <rileydnorris@gmail.com>
2017-10-16 22:26:27 -0500

    Adding a blank project file

---
+++ liftr-project-file/liftr-project-file/liftr-project-filePage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace liftrprojectfile
+{
+    public partial class liftr_project_filePage : ContentPage
+    {
+        public liftr_project_filePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
